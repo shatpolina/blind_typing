9146a77e3eca5157d74c73291a7960deca298da8
polina shat <shat.polina@gmail.com>
2022-05-02 03:21:02 +0300

    Text typing in eng, input-text/text animation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import curses
+import time
 from curses import wrapper
+
 
 def logo(stdscr):
     stdscr.addstr(5, 55, "~* Blind typing", curses.color_pair(1))
@@ -9,29 +11,65 @@ def logo(stdscr):
     stdscr.addstr(6, 72, "trainer *~")
 
 
-def text_runner(stdscr):
-    text = "Съешь ещё этих мягких французских булок, да выпей [же] чаю."
-    for index, t in enumerate(text):
-        t_input = "ы"
-        stdscr.addstr(20, 35, f"{text[:index]}", curses.color_pair(1))
-        stdscr.addstr(21, int(35 + 23), f"{text[23:]}")
-        if index == 23:
-            stdscr.addstr(20, 35 + index, f"{t_input}", curses.color_pair(2))
+def start_menu(stdscr):
+    logo(stdscr)
+    stdscr.addstr(20, 55, "Press Enter to start")
 
-            break
+
+def text_runner(stdscr):
+    # text = "Съешь ещё этих мягких французских булок, да выпей [же] чаю."
+    text = "The quick brown fox jumps over the lazy dog."
+    next = False
+    for index, t in enumerate(text):
+        stdscr.clear()
+        next = False
+        stdscr.addstr(20, 35, f"{text[:index]}", curses.color_pair(1))
+        stdscr.addstr(21, 35 + index, f"{text[index:]}")
+        stdscr.refresh()
+        while not next:
+            t_input = stdscr.getkey()
+            if t_input == t:
+                next = True
+            elif t_input != t and t_input != "KEY_BACKSPACE":
+                    stdscr.addstr(20, 35 + index, f"{t_input}", curses.color_pair(2))
+                    stdscr.refresh()
+            elif t_input == "KEY_BACKSPACE":
+                stdscr.addstr(20, 35 + index, " ", curses.color_pair(1))
+                stdscr.refresh()
+            elif t_input == "\n":
+                stdscr.clear()
+                stdscr.refresh()
+                exit()
+            else:
+                pass
 
 
 def main(stdscr):
     curses.use_default_colors()
+    curses.cbreak()
+    curses.noecho()
+    stdscr.keypad(True)
+
     black_green = curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
     black_red = curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
-    black_white = curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    black_white = curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+    start_menu(stdscr)
+
+    while True:
+        key = stdscr.getkey()
+        print(key)
+        if key == "\n":
+            break
 
     stdscr.clear()
-    logo(stdscr)
-    text_runner(stdscr)
     stdscr.refresh()
-    stdscr.getch()
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(1, 1, "Press Enter to stop")
+        text_runner(stdscr)
+        stdscr.refresh()
 
 
 wrapper(main)
